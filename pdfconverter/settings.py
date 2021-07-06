e6bd93254037f1a694a93a55afcce1cd244fd8e5
Payal Sasmal <sasmalpayal@gmail.com>
2021-07-06 21:50:26 +0530

    added files for heroku

--- pdfconverter/settings.py
+++ pdfconverter/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'pdfconverter.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
