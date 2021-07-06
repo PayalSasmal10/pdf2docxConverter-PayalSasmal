30859e74e8e872bd4e26e6d092eb859180c23b52
Payal Sasmal <sasmalpayal@gmail.com>
2021-07-06 21:05:23 +0530

    added files for heroku

--- pdfconverter/settings.py
+++ pdfconverter/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*s9a13o1z!mv^et#2evztaui*$=l#kw@oc$=b)r#m^r=n@#-)b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- pdfconverter/urls.py
+++ pdfconverter/urls.py
@@ -20,11 +20,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from .pdfapp import views
+from django.views.static import serve
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.uploadfile, name='uploadfile'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
 
 if settings.DEBUG:
     urlpatterns == static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
